54a6e1d16d9fb83521d7f34792e1fa2b215c7eaf
pranav <59198870+Pranav2008-droid@users.noreply.github.com>
2020-02-06 17:19:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,24 +1,43 @@
-
-
-
+var canvas, backgroundImage;
 
 var gameState = 0;
-var playerCount = 0;
+var playerCount;
+
 var database;
-var form;
-var player;
-var game;
-var canvas1, bgimage;
+
+var form, player, game;
+
+var allPlayers;
 
 function setup(){
-  canvas1 = createCanvas(windowWidth, windowHeight);
+
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyChF62JRjU-qEXSn3W23cQIjIuOEishx6I",
+      authDomain: "test-whitehat-ball.firebaseapp.com",
+      databaseURL: "https://test-whitehat-ball.firebaseio.com",
+      projectId: "test-whitehat-ball",
+      storageBucket: "test-whitehat-ball.appspot.com",
+      messagingSenderId: "679049809667",
+      appId: "1:679049809667:web:ee900396bb2e2b475f2336"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+  canvas = createCanvas(400,400);
   database = firebase.database();
   game = new Game();
   game.getState();
   game.start();
-  
 }
 
-function draw(){
 
+function draw(){
+  if (playerCount === 4){
+    game.update(1);
+  }
+  if (gameState === 1){
+    clear();
+    game.play();
+  }
 }
