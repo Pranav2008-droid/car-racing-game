1689755ed0b303487a3dfdd0803e5a6c08611efb
pranav <59198870+Pranav2008-droid@users.noreply.github.com>
2020-02-10 20:26:21 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,20 +9,12 @@ var form, player, game;
 
 var allPlayers;
 
+var distance = 0;
+
+
 function setup(){
 
-    // Your web app's Firebase configuration
-    var firebaseConfig = {
-      apiKey: "AIzaSyChF62JRjU-qEXSn3W23cQIjIuOEishx6I",
-      authDomain: "test-whitehat-ball.firebaseapp.com",
-      databaseURL: "https://test-whitehat-ball.firebaseio.com",
-      projectId: "test-whitehat-ball",
-      storageBucket: "test-whitehat-ball.appspot.com",
-      messagingSenderId: "679049809667",
-      appId: "1:679049809667:web:ee900396bb2e2b475f2336"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+
 
   canvas = createCanvas(400,400);
   database = firebase.database();
